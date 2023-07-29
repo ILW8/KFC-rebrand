7ea9f0c70eaac7e7761ffcff37db2f8c8f03a044
hburn7 <hburnett777@gmail.com>
2023-07-29 11:09:49 -0400

    Add basic Entity & Registrant entities

---
+++ API/Entities/Registrant.cs
@@ -0,0 +1,9 @@
+﻿namespace API.Entities;
+
+public class Registrant : EntityBase
+{
+	public long OsuId { get; set; }
+	public string OsuName { get; set; } = null!;
+	public long DiscordId { get; set; }
+	public string DiscordName { get; set; } = null!;
+}
