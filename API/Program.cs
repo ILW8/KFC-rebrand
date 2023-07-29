230856fdd66a1226f0639e3bd0357ffd668a7359
hburn7 <hburnett777@gmail.com>
2023-07-29 10:38:04 -0400

    Fetch configuration string & init serilog

--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,6 @@
+using Serilog;
+using Serilog.Events;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,18 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddSerilog(configuration =>
+{
+	string connString = builder.Configuration.GetConnectionString("DefaultConnection") ?? 
+	                    throw new InvalidOperationException("Missing connection string!");
+	configuration.MinimumLevel.Debug()
+	             .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+	             .Enrich.FromLogContext()
+	             .WriteTo.Console()
+	             .WriteTo.File("logs\\log.txt", rollingInterval: RollingInterval.Day)
+	             .WriteTo.PostgreSQL(connString, "Logs", needAutoCreateTable: true);
+});
 
 var app = builder.Build();
 
