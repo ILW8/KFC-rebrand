534bd79cfa617d9198ab0f663fd69cef9f34e141
hburn7 <hburnett777@gmail.com>
2023-07-29 10:53:12 -0400

    fix naming

--- db-test.sh
+++ db-test.sh
@@ -1,1 +1,1 @@
-docker run -d -p 5432:5432 --name db-test -e POSTGRES_PASSWORD=postgres -v 5wc2024api-test:/var/lib/postgresql postgres
+docker run -d -p 5432:5432 --name 5wc2024-db-test -e POSTGRES_PASSWORD=postgres -v 5wc2024db-test:/var/lib/postgresql postgres
