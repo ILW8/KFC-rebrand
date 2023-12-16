9a2adf5dcea1b547165bd6e53b6bc0033b12d43a
Dao Heng Liu <daohengliu@gmail.com>
2023-12-16 20:28:29 +0000

    allow setting pagination limit

--- fivedigitworldcup/settings.py
+++ fivedigitworldcup/settings.py
@@ -168,7 +168,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.AllowAny'
     ],
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'DEFAULT_PAGINATION_CLASS': 'fivedigitworldcup.pagination.PageNumberWithLimitPagination',
     'PAGE_SIZE': 50
 }
 
